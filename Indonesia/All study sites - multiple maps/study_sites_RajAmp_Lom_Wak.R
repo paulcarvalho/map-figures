7911ef3cb7645686fcfc388161140de93da05184
paulcarvalho <paulcarvalho@uri.edu>
2018-09-22 14:13:38 -0400

    added wakatobi sites

--- Indonesia/All study sites - multiple maps/study_sites_RajAmp_Lom_Wak.R
+++ Indonesia/All study sites - multiple maps/study_sites_RajAmp_Lom_Wak.R
@@ -45,21 +45,21 @@ map.plot
 raja_ampat <- coord %>% filter(region == "Raja_Ampat")
 
 # Plot Raja Ampat without sites
-map      <- get_map(location=c(lon=130.55, lat=-0.5), zoom="auto", 
-                    maptype="toner-background", source="stamen", crop=TRUE)
+map <- get_map(location=c(lon=130.55, lat=-0.5), zoom="auto", 
+               maptype="toner-background", source="stamen", crop=TRUE)
 map.plot <- ggmap(map) +
   labs(x="Longitude", y="Latitude") +           # label axes
   theme(panel.border = element_rect(colour="black", fill=NA, size=2)) # add a border around the plot
 map.plot
 
 # Plot Raja Ampat with sites
-map      <- get_map(location=c(lon=130.55, lat=-0.5), zoom="auto", 
+map <- get_map(location=c(lon=130.55, lat=-0.5), zoom="auto", 
                     maptype="toner-background", source="stamen", crop=TRUE)
 map.plot <- ggmap(map) +
   geom_point(data=raja_ampat, aes(x=lon, y=lat),   # plot site coordinates
              size=2, alpha=1, colour="red") + # plot site coordinates
   labs(x="Longitude", y="Latitude") +           # label axes
-  theme(panel.border = element_rect(colour="black", fill=NA, size=2)) + # add a border around the plot
+  theme(panel.border = element_rect(colour="black", fill=NA, size=2)) # add a border around the plot
 # map.plot$layers[2][[1]]$geom_params$raster <- gsub("#000000", "#AFB0B1", map.plot$layers[2][[1]]$geom_params$raster) # change the color of the ocean in the raster
 map.plot
 
@@ -67,16 +67,24 @@ map.plot
 #____________________________________________________________________________________________________________________
 #### Wakatobi Map ####
 #____________________________________________________________________________________________________________________
-# Plot sites on a map
-loc      <- c(123.7, -5.65)
-map      <- get_map(location=loc, zoom=10, source= "stamen", maptype="toner-background", crop=FALSE)
+# Filter data
+wakatobi <- coord %>% filter(region == "Wakatobi")
 
+# Plot Wakatobi without sites
+map <- get_map(location=c(123.7, -5.6), zoom="auto",
+               source= "stamen", maptype="toner-background", crop=FALSE)
 map.plot <- ggmap(map) +
-  geom_point(data=fish.data, aes(x=long, y=lat), size=2, colour="black") +
   labs(x="Longitude", y="Latitude") +
   theme(panel.border = element_rect(colour="black", fill=NA, size=2)) # add a border around the plot
-map.plot$layers[2][[1]]$geom_params$raster <- gsub("#000000", "#AFB0B1", map.plot$layers[2][[1]]$geom_params$raster) # change the hexadecimal color of the ocean in the raster
-# change the hexadecimal color of the ocean in the raster
+map.plot
+
+# Plot Wakatobi with sites
+map <- get_map(location=c(123.7, -5.6), zoom="auto",
+               source= "stamen", maptype="toner-background", crop=FALSE)
+map.plot <- ggmap(map) +
+  geom_point(data=wakatobi, aes(x=lon, y=lat), size=2, colour="red") +
+  labs(x="Longitude", y="Latitude") +
+  theme(panel.border = element_rect(colour="black", fill=NA, size=2)) # add a border around the plot
 map.plot
 
 
