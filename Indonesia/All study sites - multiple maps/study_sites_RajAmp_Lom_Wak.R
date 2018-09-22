a66553be62106684471cf1ad6769ac69d5a569b6
paulcarvalho <paulcarvalho@uri.edu>
2018-09-22 13:29:34 -0400

    added map for Raja Ampat

--- Indonesia/All study sites - multiple maps/study_sites_RajAmp_Lom_Wak.R
+++ Indonesia/All study sites - multiple maps/study_sites_RajAmp_Lom_Wak.R
@@ -8,6 +8,12 @@
 
 rm(list=ls())
 
+#____________________________________________________________________________________________________________________
+#### DIRECTORIES ####
+#____________________________________________________________________________________________________________________
+# 
+setwd("~/github/map-figures/Indonesia/All study sites - multiple maps")
+
 
 #____________________________________________________________________________________________________________________
 #### Libraries ####
@@ -16,6 +22,12 @@ library(ggplot2)
 library(ggmap)
 library(maps)
 library(mapdata)
+
+
+#____________________________________________________________________________________________________________________
+#### Import data ####
+#____________________________________________________________________________________________________________________
+coord <- read_excel("site_info.xlsx")
 
 
 #____________________________________________________________________________________________________________________
@@ -29,21 +41,27 @@ map.plot
 #____________________________________________________________________________________________________________________
 #### Raja Ampat Map ####
 #____________________________________________________________________________________________________________________
-# Plot sites on a map
-map      <- get_map(location=c(lon=130.2, lat=-0.5), zoom=9, 
-                    maptype="toner-background", source="stamen", crop=FALSE)
+# Filter data
+raja_ampat <- coord %>% filter(region == "Raja_Ampat")
+
+# Plot Raja Ampat without sites
+map      <- get_map(location=c(lon=130.55, lat=-0.5), zoom="auto", 
+                    maptype="toner-background", source="stamen", crop=TRUE)
 map.plot <- ggmap(map) +
-  geom_point(data=site.df, aes(x=lon, y=lat),   # plot site coordinates
-             size=2, alpha=1, colour="black") + # plot site coordinates
   labs(x="Longitude", y="Latitude") +           # label axes
-  geom_point(data=loc.df, aes(x=lon, y=lat),    # plot city coordinates
-             size=3, colour="blue", shape=18) + # plot city coordinates
+  theme(panel.border = element_rect(colour="black", fill=NA, size=2)) # add a border around the plot
+map.plot
+
+# Plot Raja Ampat with sites
+map      <- get_map(location=c(lon=130.55, lat=-0.5), zoom="auto", 
+                    maptype="toner-background", source="stamen", crop=TRUE)
+map.plot <- ggmap(map) +
+  geom_point(data=raja_ampat, aes(x=lon, y=lat),   # plot site coordinates
+             size=2, alpha=1, colour="red") + # plot site coordinates
+  labs(x="Longitude", y="Latitude") +           # label axes
   theme(panel.border = element_rect(colour="black", fill=NA, size=2)) + # add a border around the plot
-  annotate('rect', xmin=131.2, ymin=-0.8, xmax=131.5, ymax=-0.6, col="black", fill=NA) + # annotate "Sorong"
-  annotate('text', x=131.2, y=-0.8, label='Sorong', colour="black", size=5) +            # annotate "Sorong"
-  annotate('rect', xmin=131, ymin=-0.45, xmax=131.5, ymax=-0.6, col="black", fill=NA) +  # annotate "Waisai"
-  annotate('text', x=131, y=-0.45, label='Waisai', colour="black", size=5)               # annotate "Waisai"
-map.plot$layers[2][[1]]$geom_params$raster <- gsub("#000000", "#AFB0B1", map.plot$layers[2][[1]]$geom_params$raster) # change the color of the ocean in the raster
+# map.plot$layers[2][[1]]$geom_params$raster <- gsub("#000000", "#AFB0B1", map.plot$layers[2][[1]]$geom_params$raster) # change the color of the ocean in the raster
+map.plot
 
 
 #____________________________________________________________________________________________________________________
@@ -60,6 +78,11 @@ map.plot <- ggmap(map) +
 map.plot$layers[2][[1]]$geom_params$raster <- gsub("#000000", "#AFB0B1", map.plot$layers[2][[1]]$geom_params$raster) # change the hexadecimal color of the ocean in the raster
 # change the hexadecimal color of the ocean in the raster
 map.plot
+
+
+#____________________________________________________________________________________________________________________
+#### Lombok Map ####
+#____________________________________________________________________________________________________________________
 
 
 
